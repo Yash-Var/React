aeb546a19a9575b00b0dbdf58adcde5b60a733c8
Yash Varshney <yashvarshneypepsi@gmail.com>
2022-06-22 19:45:47 +0530

    complete first app by react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM  from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+let curDate=new Date();
+let greeting='';
+curDate=curDate.getHours();
+const csstyle={ };
+if(curDate>=1 && curDate<12){
+greeting='Good Morning';
+ csstyle.color="green";
+}else if(curDate>=12 && curDate<19)
+{
+  greeting='Good Afternoon';
+  csstyle.color="orange";
+}else{
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  greeting='Good Night';
+  csstyle.color="blue";
+}
+ReactDOM.render(<>
+<div>
+<h1>Hello sir, <span style={csstyle}>{greeting}</span></h1>
+</div>
+</>
+,
+document.getElementById('root'));
